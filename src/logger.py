910dee6389813ced7294830034e29cb7ef6a985f
Dhruva Saxena <saxena08dhruv@outlook.com>
2023-07-23 15:39:29 +0530

    Eda and Problem Statement

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,6 @@ import logging
 import os
 from datetime import datetime
 
-
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
@@ -16,6 +15,6 @@ logging.basicConfig(
     level= logging.INFO
 )
 
-if __name__=="__main__":
+# if __name__=="__main__":
 
-    logging.info("Logging has started")
+#     logging.info("Logging has started")
